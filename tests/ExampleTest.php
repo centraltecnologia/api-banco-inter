1709aa3f63ba26f5703e138e75ed34ffba2cf8ba
Rodrigo Pires Meira <rpmeir@gmail.com>
2021-10-11 23:55:23 -0300

    Use Laravel Lumen

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,21 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
-});
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->get('/');
+
+        $this->assertEquals(
+            $this->app->version(), $this->response->getContent()
+        );
+    }
+}
